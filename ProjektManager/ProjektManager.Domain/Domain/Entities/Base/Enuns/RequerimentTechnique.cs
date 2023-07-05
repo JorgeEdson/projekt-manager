d47a03e95edcf6f1feb2403d3c3d6844df7f318d
Jorge Edson <jorge.edson.bsi@gmail.com>
2023-07-05 13:07:42 -0300

    created enum RequerimentTechnique

---
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/Base/Enuns/RequerimentTechnique.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektManager.Core.Domain.Entities.Base.Enuns
+{
+    enum RequerimentTechnique
+    {
+        UseCase,
+        UserStory
+    }
+}
