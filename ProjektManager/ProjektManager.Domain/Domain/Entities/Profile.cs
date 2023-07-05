94e20decb8cce279b25cfd60176ed8b464fc1c8b
blcomedyg <brenosilvalima03@gmail.com>
2023-07-05 20:42:46 -0300

    .

--- ProjektManager/ProjektManager.Domain/Domain/Entities/Profile.cs
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/Profile.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using ProjektManager.Core.Domain.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjektManager.Core.Domain.Entities
 {
-    internal class Profile
+    public class Profile:BaseEntity
     {
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
     }
 }
