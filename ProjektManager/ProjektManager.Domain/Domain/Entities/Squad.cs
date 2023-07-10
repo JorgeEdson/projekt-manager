5d90615c0e3ebe62e591bbb9127c74aae60fd8fd
blcomedyg <brenosilvalima03@gmail.com>
2023-07-10 20:41:42 -0300

    Add user context classes

---
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/Squad.cs
@@ -0,0 +1,13 @@
+﻿
+using ProjektManager.Core.Domain.Entities.Base;
+
+namespace ProjektManager.Core.Domain.Entities
+{
+    public class Squad:BaseEntity
+    {
+        public List<UserProfile> UsersProfiles { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
---
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿
+using ProjektManager.Core.Domain.Entities.Base;
+
+namespace ProjektManager.Core.Domain.Entities
+{
+    public class User:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Profile> Profiles { get; set; }
+
+    }
+}
