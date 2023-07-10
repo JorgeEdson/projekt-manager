b3fa7471dec6352e9cd60da200d04d536496e5ad
blcomedyg <brenosilvalima03@gmail.com>
2023-07-10 18:50:23 -0300

    Adding user context classes.

---
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using ProjektManager.Core.Domain.Entities.Base;
+
+namespace ProjektManager.Core.Domain.Entities
+{
+    public class UserProfile:BaseEntity
+    {
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public List<Squad> Squads { get; set; }
+
+    }
+}
