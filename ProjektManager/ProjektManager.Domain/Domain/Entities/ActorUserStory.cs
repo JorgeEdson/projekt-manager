f8114b18040c8b97a2a8bcbed39e7b30f2d2116b
gomesfilho-developer <gomesfilho.developer@gmail.com>
2023-07-10 23:20:46 -0300

    Add class ActorUserStory

---
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/ActorUserStory.cs
@@ -0,0 +1,17 @@
+﻿using ProjektManager.Core.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektManager.Core.Domain.Entities
+{
+    public class ActorUserStory: BaseEntity
+    {
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+        public int UserStoryId { get; set; }
+        public UserStory UserStory { get; set; }
+    }
+}
