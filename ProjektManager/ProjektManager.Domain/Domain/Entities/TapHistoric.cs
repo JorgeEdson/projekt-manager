5fc5e0fb272c75462e39bcf2da03f4dc56619766
Jorge Edson <jorge.edson.bsi@gmail.com>
2023-07-10 22:07:34 -0300

    add context Tap classes

---
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/TapHistoric.cs
@@ -0,0 +1,21 @@
+﻿using ProjektManager.Core.Domain.Entities.Base;
+
+namespace ProjektManager.Core.Domain.Entities
+{
+    public class TapHistoric : BaseEntity
+    {
+        public int TapId { get; set; }
+        public Tap Tap { get; set; }
+        public Project Project { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Objective> Objectives { get; set; }
+        public List<Justification> Justifications { get; set; }
+        public List<Stakeholder> Stakeholder { get; set; }
+        public List<Premise> Premises { get; set; }
+        public List<Restriction> Restrictions { get; set; }
+        public List<ExcludedEscoped> ExcludedEscopes { get; set; }
+        public List<Risk> Riskiness { get; set; }
+        public List<PaymentAgreement> PaymentAgreements { get; set; }
+    }
+}
---
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/TapSession.cs
@@ -0,0 +1,11 @@
+﻿using ProjektManager.Core.Domain.Entities.Base;
+
+namespace ProjektManager.Core.Domain.Entities
+{
+    public abstract class TapSession : BaseEntity
+    {
+        public string Description { get; set; }
+        public int TapId { get; set; }
+        public Tap Tap { get; set; }
+    }
+}
