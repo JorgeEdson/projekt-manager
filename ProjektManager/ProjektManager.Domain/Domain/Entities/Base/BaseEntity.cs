2d0f04f067f69e4c87a38d727db0230da096c6f2
Jorge Edson <jorge.edson.bsi@gmail.com>
2023-08-06 07:47:30 -0300

    operations in class Profile

--- ProjektManager/ProjektManager.Domain/Domain/Entities/Base/BaseEntity.cs
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/Base/BaseEntity.cs
@@ -9,9 +9,17 @@ namespace ProjektManager.Core.Domain.Entities.Base
 {
     public abstract class BaseEntity
     {
-        public Guid Id { get; private set; }
+        public Guid? Id { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public Guid UserId { get; private set; }
         public User User { get; private set; }
+
+        protected BaseEntity(DateTime createdAt, User user)
+        {
+            CreatedAt = createdAt;
+            User = user;
+            UserId = user.UserId;
+        }
+        protected BaseEntity(){}
     }
 }
--- ProjektManager/ProjektManager.Domain/Domain/Entities/Profile.cs
+++ ProjektManager/ProjektManager.Domain/Domain/Entities/Profile.cs
@@ -4,8 +4,26 @@ namespace ProjektManager.Core.Domain.Entities
 {
     public class Profile:BaseEntity
     {
+        #region Properties
         public string Name { get; private set; }
+        public List<User>? Users { get; private set; }
+        #endregion
 
-        public List<User> ? Users { get; private set; }
+        #region Constructors
+        public Profile(DateTime createdAt, User user, string name) :base(createdAt, user)
+        {
+            Name = name;
+        }
+        public Profile()
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
     }
 }
